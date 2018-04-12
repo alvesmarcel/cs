4980a3d16ab0a7e92dda45a568f690991961bc08
Marcel Oliveira Alves <marcel.alves89@gmail.com>
2018-04-12 10:29:35 -0300

    Reorganize file structure. Add Merge Sort implementation.

---
+++ Algorithms/Sorting/Insertion Sort/insertionSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include "../Test.hpp"
+
+void insertionSort(std::vector<int> &v) {
+    for (int i = 1; i < v.size(); i++) {
+        int value = v[i];
+        int j = i - 1;
+        while (j >= 0 && v[j] > value) {
+            v[j+1] = v[j];
+            j--;
+        }
+        v[j+1] = value;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    for (int i = 0; i < 10; i++) {
+        std::vector<int> v = Test::generateRandomVector();
+        std::vector<int> original = v;
+        std::vector<int> expected = v;
+        std::sort(expected.begin(), expected.end());
+        insertionSort(v);
+        if (!Test::isSorted(v)) {
+            std::cout << "Insertion Sort failed:\n";
+            std::cout << "Original: ";
+            Test::printVector(original);
+            std::cout << "Expected: ";
+            Test::printVector(expected);
+            std::cout << "Result: ";
+            Test::printVector(v);
+            std::cout << std::endl;
+        }
+    }
+    
+    std::vector<std::vector<int>> specialCases = {{}, {0}, {0, 0, 0, 0, 0, 0, 0}, {0, 1, 2, 3, 4, 5, 6}, {6, 5, 4, 3, 2, 1, 0}};
+    for (int i = 0; i < specialCases.size(); i++) {
+        insertionSort(specialCases[i]);
+        std::vector<int> original = specialCases[i];
+        std::vector<int> expected = specialCases[i];
+        std::sort(expected.begin(), expected.end());
+        if (!Test::isSorted(specialCases[i])) {
+            std::cout << "Insertion Sort failed:\n";
+            std::cout << "Original: ";
+            Test::printVector(original);
+            std::cout << "Expected: ";
+            Test::printVector(expected);
+            std::cout << "Result: ";
+            Test::printVector(specialCases[i]);
+            std::cout << std::endl;
+        }
+    }
+    
+    std::cout << "Insertion Sort successful\n";
+    
+    return 0;
+}
--- Algorithms/Sorting/Merge Sort/mergeSort.cpp
+++ Algorithms/Sorting/Merge Sort/mergeSort.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
 #include <vector>
-#include "Test.hpp"
+#include "../Test.hpp"
 
-void insertionSort(std::vector<int> &v) {
-    for (int i = 1; i < v.size(); i++) {
-        int value = v[i];
-        int j = i - 1;
-        while (j >= 0 && v[j] > value) {
-            v[j+1] = v[j];
-            j--;
+void merge(std::vector<int> &v, int left, int mid, int right) {
+    //int mid = (left + right) / 2;
+    int i = left;
+    int j = mid + 1;
+    
+    std::vector<int> aux{};
+    
+    // Performs the usual merge
+    while (i <= mid && j <= right) {
+        if (v[i] < v[j]) {
+            aux.push_back(v[i]);
+            ++i;
+        } else {
+            aux.push_back(v[j]);
+            ++j;
         }
-        v[j+1] = value;
+    }
+    
+    // Insert the remaining elements in the range [left, mid] in aux vector
+    while (i <= mid) {
+        aux.push_back(v[i]);
+        ++i;
+    }
+    
+    // Insert the remaining elements in the range [mid+1, right] in aux vector
+    while (j <= right) {
+        aux.push_back(v[j]);
+        ++j;
+    }
+    
+    // Copy the elements from aux to v
+    for (int i = left; i <= right; ++i) {
+        v[i] = aux[i-left];
+    }
+}
+
+void mergeSort(std::vector<int> &v, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(v, left, mid);
+        mergeSort(v, mid+1, right);
+        merge(v, left, mid, right);
     }
 }
 
@@ -21,9 +54,9 @@ int main(int argc, const char * argv[]) {
         std::vector<int> original = v;
         std::vector<int> expected = v;
         std::sort(expected.begin(), expected.end());
-        insertionSort(v);
+        mergeSort(v, 0, (int)v.size()-1);
         if (!Test::isSorted(v)) {
-            std::cout << "Insertion Sort failed:\n";
+            std::cout << "Merge Sort failed:\n";
             std::cout << "Original: ";
             Test::printVector(original);
             std::cout << "Expected: ";
@@ -36,12 +69,12 @@ int main(int argc, const char * argv[]) {
     
     std::vector<std::vector<int>> specialCases = {{}, {0}, {0, 0, 0, 0, 0, 0, 0}, {0, 1, 2, 3, 4, 5, 6}, {6, 5, 4, 3, 2, 1, 0}};
     for (int i = 0; i < specialCases.size(); i++) {
-        insertionSort(specialCases[i]);
+        mergeSort(specialCases[i], 0, (int)specialCases[i].size()-1);
         std::vector<int> original = specialCases[i];
         std::vector<int> expected = specialCases[i];
         std::sort(expected.begin(), expected.end());
         if (!Test::isSorted(specialCases[i])) {
-            std::cout << "Insertion Sort failed:\n";
+            std::cout << "Merge Sort failed:\n";
             std::cout << "Original: ";
             Test::printVector(original);
             std::cout << "Expected: ";
@@ -52,7 +85,7 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    std::cout << "Insertion Sort successful\n";
+    std::cout << "Merge Sort successful\n";
     
     return 0;
 }
